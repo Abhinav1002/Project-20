ecf4251e851af33deb13e91b768fc29a71d79c68
Abhinav1002 <72290507+Abhinav1002@users.noreply.github.com>
2020-10-19 20:23:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,48 +1,39 @@
+var car,wall;
+var speed,weight;
+
 function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-  movingRect = createSprite(400,200,80,30);
-  movingRect.shapeColor = "green";
-  movingRect.debug = true;
-
-  gameObject1 = createSprite(100, 100, 50, 50);
-  gameObject1.shapeColor = "green";
-  gameObject2 = createSprite(200, 100, 50, 50);
-  gameObject2.shapeColor = "green";
-  gameObject3 = createSprite(300, 100, 50, 50);
-  gameObject3.shapeColor = "green";
-  gameObject4 = createSprite(400, 100, 50, 50);
-  gameObject4.shapeColor = "green";
-
+  createCanvas(1600,400);
+  speed = random(50,90);
+  weight = random(400,1500);
+  car=createSprite(50,200,50,50);
+  wall=createSprite(1300,200,60,height/2);
+  wall.color (80,80,80);
+  car.velocityX = speed;
 }
+
+
 function draw() {
-  background("black");  
-  gameObject1.x = World.mouseX;
-  gameObject1.y = World.mouseY;
+  background(0);
 
-if(isTouching( gameObject1,gameObject2)){
+    if(wall.x-car.x < (car.width+wall.width)/2)
+{
+   car.velocityX=0;
+   var deformation=0.5 * weight * speed * speed/22509;
+   if(deformation>180)
+   {
+       wall.shapeColor=color(255,0,0);
+   }
+   if(deformation<180 && deformation>100)
+  {
+       car.shapeColor=color(230,230,0);
+  }
+   
+   if(deformation<100)
+{
+    car.shapeColor=color(0,255,0);
+}
+}
+
   
-gameObject1.shapeColor = "blue";
-gameObject2.shapeColor = "blue";
-}
-else {
-  gameObject1.shapeColor = "green";
-gameObject2.shapeColor = "green";
-}
   drawSprites();
-}
-
-function isTouching(object1,object2){
-  if (object1.x - object2.x  < object2.width/2 + object1.width/2 && 
-    object2.x-object1.x  < object2.width/2 + object1.width/2 && 
-    object1.y - object2.y  < object2.height/2 + object1.height/2 &&
-     object2.y - object1.y  < object2.height/2 + object1.height/2)
-    {
-      return true;
-    }
-    else {
-      return false;
-    }
 }
